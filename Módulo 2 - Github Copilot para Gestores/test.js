52b45a91c7028f06a8cca49293cbdc2f2fe6dca7
Ahirton Lopes <ahirtonlopes@gmail.com>
2025-02-07 17:23:39 -0300

    Delete Módulo 2 - Github Copilot para Gestores/test.js

--- Módulo 2 - Github Copilot para Gestores/test.js
+++
@@ -1,7 +0,0 @@
-function calculateDaysBetweenDates(begin, end) {
-    const beginDate = new Date(begin);
-    const endDate = new Date(end);
-    const timeDifference = endDate.getTime() - beginDate.getTime();
-    return timeDifference / (1000 * 3600 * 24);
-}
-
