e7bd764aab648082a86e0dfa9d5382e040ac903e
Ahirton Lopes <ahirtonlopes@gmail.com>
2025-02-12 16:42:34 -0300

    Add files via upload

---
+++ Módulo 4 - Uso Avançado do GitHub Copilot/Aula 8/test4.py
@@ -0,0 +1,13 @@
+# 4. Testes Automatizados
+
+def calcular_tempo_entrega(distancia):
+    if distancia <= 50:
+        return 1
+    elif distancia <= 200:
+        return 2
+    else:
+        return 3
+
+# Peça para o CoPilot criar um arquivo de teste test_entrega.py.
+# Utilizar o Copilot para gerar testes unitários com o framework pytest.
+# Executar os testes e validar os resultados.
